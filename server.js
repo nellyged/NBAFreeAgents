80483ef72da879a29d8b5dbb64e565db454d28a1
nelsonjlgedeon <nelson.gedeon@gmail.com>
2019-02-09 23:14:31 -0500

    Bring Back API

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const db = require('./db');
-const api = require('./api');
+//const api = require('./api');
 const app = require('./app');
-const apiPort = process.env.PORT || 3001;
+//const apiPort = process.env.PORT || 3001;
 const appPort = process.env.PORT || 1337;
-api.listen(apiPort, () => console.log(`api listening on port ${apiPort}`));
+//api.listen(apiPort, () => console.log(`api listening on port ${apiPort}`));
 app.listen(appPort, () => console.log(`api listening on port ${appPort}`));
 db.syncAndSeed();
